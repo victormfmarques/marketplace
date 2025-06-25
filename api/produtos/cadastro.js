e144a6910159460ee2c7de61803e7d5683709c89
Victor Manoel <148561449+victormfmarques@users.noreply.github.com>
2025-06-25 17:16:22 -0300

    Update cadastro.js

--- api/produtos/cadastro.js
+++ api/produtos/cadastro.js
@@ -8,9 +8,9 @@ const client = new MongoClient(uri);
 
 // Configure o Cloudinary (serviço gratuito para imagens)
 cloudinary.config({ 
-  cloud_name: process.env.CLOUDINARY_NAME, 
-  api_key: process.env.CLOUDINARY_KEY,
-  api_secret: process.env.CLOUDINARY_SECRET 
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET 
 });
 
 export default async function handler(req, res) {
